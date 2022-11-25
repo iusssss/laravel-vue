adfd87c96ce0da19a9931e246014c985d034477c
Julius Decena <juliusd@surefirelocal.com>
2022-11-26 02:59:01 +0800

    Fixed frontend authentication

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,8 +31,6 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>test vue</div>
+  <div class="container m-5">
+    <button class="btn btn-primary" @click="login">Login</button>
+  </div>
 </template>
 
 <script>
-// import { defineComponent } from "@vue/composition-api";
-
 export default {
-  setup() {},
+  setup() {
+    async function login() {
+      //   await axios.get("/sanctum/csrf-cookie");
+      //   await axios.post("/login", {
+      //     email: "admin@test.com",
+      //     password: "password",
+      //   });
+
+      const { data } = await axios.get("/api/user");
+      console.log(data);
+    }
+
+    return {
+      login,
+    };
+  },
 };
 </script>
 
