27db8ad4cf6b83e936d8413eb7417ea425f8457a
Julius Decena <juliusd@surefirelocal.com>
2022-11-25 23:51:42 +0800

    Configured vuejs and bootstrap css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+import { createApp } from 'vue'
+import App from './components/App.vue'
 
-window.Alpine = Alpine;
-
-Alpine.start();
+createApp(App).mount("#app")
---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container p-3">test vue</div>
+</template>
+
+<script>
+// import { defineComponent } from "@vue/composition-api";
+
+export default {
+  setup() {},
+};
+</script>
+
+<style scoped>
+</style>
